693a53ca88f13862b92df4085dce7308abdfe80d
Ken McEnaney <kxm@creare.com>
2019-02-01 23:05:17 -0500

    Add TympanRemote provider to second tab.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TympanRemote } from '../providers/tympan-remote';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,7 +18,8 @@ import { AppComponent } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    TympanRemote
   ],
   bootstrap: [AppComponent]
 })
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { TympanRemote } from '../../providers/tympan-remote';
+import { TympanDevice } from '../../providers/tympan-device';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+    public device: TympanDevice;
+
+    constructor(public remote: TympanRemote) {
+        this.device = this.remote.devices[0];
+    }
+
+    cmd(s: string) {
+        this.device.sendCommand(s);
+    }
+}
