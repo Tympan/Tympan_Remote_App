9375ab924eacd0aa58980d80ef59572f72bfac1e
Ken McEnaney <kxm@creare.com>
2019-02-17 18:13:19 -0500

    Setting up the three basic tabs

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { TympanRemote } from '../../providers/tympan-remote';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+    constructor(public remote: TympanRemote) {
+    }
+
+    cmd(s: string) {
+        this.remote.send(s);
+    }
+}
