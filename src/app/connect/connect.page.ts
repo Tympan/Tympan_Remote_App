a4947d518fe692c611d4c66b46eebedf4ef059d7
rnasser <rnasser@creare.com>
2019-08-20 09:05:59 -0400

    Added AppVersion plugin and made so version number will be taken from config file and displayed on Connect page

--- src/app/connect/connect.page.ts
+++ src/app/connect/connect.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TympanRemote } from '../../providers/tympan-remote';
 import { Logger } from '../../providers/logger';
+import { AppVersion } from '@ionic-native/app-version/ngx';
 
 @Component({
   selector: 'app-tab3',
@@ -8,10 +9,20 @@ import { Logger } from '../../providers/logger';
   styleUrls: ['connect.page.scss']
 })
 export class ConnectPage {
-
-    constructor(public remote: TympanRemote, public logger:Logger) {
+    VersionNumber:string;
+    constructor(public remote: TympanRemote, public logger:Logger, public appVersion: AppVersion) {
+      {
+        this.runVersion();
+      }
     }
 
+    public async runVersion(){
+      this.appVersion.getVersionNumber().then(value => {
+        this.VersionNumber = value;
+      }).catch(err => {
+        alert(err);
+      });
+    }
     public setActive(id: string) {
       this.logger.log(`Setting ${id} as active`);
       this.remote.setActiveDevice(id);
