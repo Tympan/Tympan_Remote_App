cafeb9f0a013cc8595b78610707fda7a5fef849f
Ken McEnaney <kxm@creare.com>
2019-10-14 09:49:09 -0400

    Move some definitions to a tympan-config file.

--- src/providers/tympan-remote.ts
+++ src/providers/tympan-remote.ts
@@ -3,198 +3,35 @@ import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
 import { Logger } from './logger';
 import ieee754 from 'ieee754';
 
-interface iDevice {
-  id: string;
-  name: string;
-  uuid?: string;
-  class?: number;
-  address?: string;
-  rssi?: number;
-  emulated?: boolean;
-}
+import {
+  iDevice,
+  DEVICE_1,
+  DEVICE_2,
+  DATASTREAM_START_CHAR,
+  DATASTREAM_SEPARATOR,
+  DATASTREAM_END_CHAR,
+  DATASTREAM_PREFIX_GHA,
+  DATASTREAM_PREFIX_DSL,
+  DATASTREAM_PREFIX_AFC,
+  BUTTON_STYLE_ON,
+  BUTTON_STYLE_OFF,
+  BOYSTOWN_PAGE_DSL,
+  BOYSTOWN_PAGE_WDRC,
+  BOYSTOWN_PAGE_AFC,
+  BOYSTOWN_PAGE_PLOT,
+  DEFAULT_CONFIG,
+} from './tympan-config';
 
-const DEVICE_1: iDevice = {
-  id: 'mo:ck:01',
-  name: 'mock1',
-  emulated: false
-};
-
-const DEVICE_2: iDevice = {
-  id: 'mo:ck:02',
-  name: 'mock2',
-  emulated: false
-};
+export enum BluetoothType {BLUETOOTH_SERIAL, BLE};
 
 const BLUETOOTH:boolean = true;
 
 enum ByteOrder {MSB, LSB};
 
-/* Some definitions that need to be the same in the app and in the Tympan code: */
-const DATASTREAM_START_CHAR = String.fromCharCode(0x02);
-const DATASTREAM_SEPARATOR = String.fromCharCode(0x03);
-const DATASTREAM_END_CHAR = String.fromCharCode(0x04);
-const DATASTREAM_PREFIX_GHA = 'gha';
-const DATASTREAM_PREFIX_DSL = 'dsl';
-const DATASTREAM_PREFIX_AFC = 'afc';
-
-const BUTTON_STYLE_ON = {color: 'success', isOn: true};
-const BUTTON_STYLE_OFF = {color: 'medium', isOn: false};
-
 let ADD_BOYSTOWN_DSL: boolean = false;
 let ADD_BOYSTOWN_WDRC: boolean = false;
 let ADD_BOYSTOWN_AFC: boolean = false;
 let ADD_BOYSTOWN_PLOT: boolean = false;
-
-const BOYSTOWN_PAGE_DSL = {
-  'title': 'Boys Town Algorithm',
-  'cards': [
-    {
-      'name': 'Multiband Compression',
-      'inputs': [
-        {'label': 'Attack (msec)', 'type': 'float', 'value': 30},
-        {'label': 'Release (msec)', 'type': 'float', 'value': 300},
-        {'label': 'Number of Channels (1-8)', 'type': 'int', 'value': 8, 'disabled': true},
-        {'label': 'Output at Full Scale (db SPL)', 'type': 'float', 'value': 115},
-        {'label': 'Band Data', 'type': 'grid', 'numRows': 8, 'indexLabel': 'Band', 'columns': [
-                {'label': 'Crossover Frequency (Hz)', 'type': 'int', 'values': [0, 317, 503, 798, 1265, 2006, 3181, 5045]},
-                {'label': 'Low SPL: Compression Ratio', 'type': 'float', 'values': [0.57, 0.57, 0.57, 0.57, 0.57, 0.57, 0.57, 0.57]},
-                {'label': 'Low SPL: End Knee (dB SPL)', 'type': 'float', 'values': [45.0, 45.0, 33.0, 32.0, 36.0, 34.0, 36.0, 40.0]},
-                {'label': 'Linear Region: Gain (dB)', 'type': 'float', 'values': [20., 20., 25., 30., 30., 30., 30., 30.]},
-                {'label': 'Compression: Start Knee (dB SPL)', 'type': 'float', 'values': [20., 20., 25., 30., 30., 30., 30., 30.]},
-                {'label': 'Compression: Ratio', 'type': 'float', 'values': [1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5]},
-                {'label': 'Limiter: Threshold (dB SPL)', 'type': 'float', 'values': [90., 90., 90., 90., 90., 91., 92., 93.]},
-        ]},
-      ],      
-      'submitButton': {'prefix': DATASTREAM_PREFIX_DSL}
-    },
-  ],
-};
-
-const BOYSTOWN_PAGE_WDRC = {
-      'title': 'Boys Town Algorithm',
-      'cards': [
-    {
-      'name': 'Broadband Output Compression',
-      'inputs': [
-        {'label': 'Attack (msec)', 'type': 'float', 'value': 5},
-        {'label': 'Release (msec)', 'type': 'float', 'value': 300},
-        {'label': 'Low SPL: Compression Ratio', 'type': 'float', 'value': 1.0},
-        {'label': 'Low SPL: End Knee (dB SPL)', 'type': 'float', 'value': 0.0},
-        {'label': 'Linear Region: Gain (dB)', 'type': 'float', 'value': 0.},
-        {'label': 'Compression: Start Knee (dB SPL)', 'type': 'float', 'value': 115.},
-        {'label': 'Compression: Ratio', 'type': 'float', 'value': 1.},
-        {'label': 'Limiter: Threshold (dB SPL)', 'type': 'float', 'value': 98.0},
-      ],
-      'submitButton': {'prefix': DATASTREAM_PREFIX_GHA}
-    },
-  ],
-};
-
-const BOYSTOWN_PAGE_AFC = {
-      'title': 'Boys Town Algorithm',
-      'cards': [
-    {
-      'name': 'Adaptive Feedback Cancelation',
-      'inputs': [
-        {'label': 'Enable (1=yes, 0=no)', 'type': 'int', 'value': 1},
-        {'label': 'Filter Length (samples, 0-256)', 'type': 'int', 'value': 100},
-        {'label': 'Adaptation Factor (mu, 0.0-1.0)', 'type': 'float', 'value': 0.00100},
-        {'label': 'Smoothing Factor (rho, 0.0-1.0)', 'type': 'float', 'value': 0.90},
-        {'label': 'Min Allowed Envelope (eps, 0-1.0)', 'type': 'float', 'value': 0.008},
-
-      ],
-      'submitButton': {'prefix': DATASTREAM_PREFIX_AFC}
-    },
-  ],
-};
-
-const BOYSTOWN_PAGE_PLOT = {
-      'title': 'Boys Town Algorithm',
-      'cards': [
-    {
-      'name': 'Frequency v. Output Level',
-      'plot':{}
-    },
-  ],
-};
-
-
-const DEFAULT_CONFIG = {
-  'icon': 'creare.png',
-  'pages': [
-    { 
-      'title':'Global', 
-      'cards':[
-        {
-          'name': 'Algorithm',
-          'buttons': [
-            {'label': '~A', 'cmd': 'd', 'id': 'algA'},
-            {'label': '~B', 'cmd': 'D', 'id': 'algB'},
-            {'label': '~C', 'cmd': 'c', 'id': 'algC'}
-          ]
-        }
-      ]
-    },
-    { 
-      'title':'Gain Settings', 
-      'cards':[
-        {
-          'name': 'High Gain',
-          'buttons': [
-            {'label': '~-', 'cmd': '#', 'id': 'hi'},
-            {'label': '~+', 'cmd': '3', 'id': 'rest'}
-          ]
-        },
-        {
-          'name': 'Mid Gain',
-          'buttons': [
-            {'label': '~-', 'cmd': '@', 'id': 'rest'},
-            {'label': '~+', 'cmd': '2', 'id': 'rest'}
-          ]
-        },
-        {
-          'name': 'Low Gain',
-          'buttons': [
-            {'label': '~-', 'cmd': '!'},
-            {'label': '~+', 'cmd': '1'}
-          ]
-        }
-      ]
-    },
-    { 
-      'title':'Prescription', 
-      'cards':[
-        {
-          'name': 'Additional Pages',
-          'toggles': [
-            {'label': 'Boys Town Multiband Compression', 'pagename': BOYSTOWN_PAGE_DSL, "id": ADD_BOYSTOWN_DSL},
-            {'label': 'Boys Town Broadband Output Compression', 'pagename': BOYSTOWN_PAGE_WDRC, "id": ADD_BOYSTOWN_WDRC},
-            {'label': 'Boys Town Adaptive Feedback Cancelation', 'pagename': BOYSTOWN_PAGE_AFC, "id": ADD_BOYSTOWN_AFC},
-            {'label': 'Boys Town Plot', 'pagename': BOYSTOWN_PAGE_PLOT, "id": ADD_BOYSTOWN_PLOT},
-          ],
-          'submitButton': {'prefix': 'Add Pages'}
-        },
-      ]
-    },
-  ]
-};
-
-/*
-const DEFAULT_CONFIG = {
-  'icon':'tympan.png',
-  'pages':[
-    {'title':'Presets','cards':[
-      {'name':'Record Mics to SD Card','buttons':[{'label': 'Start', 'cmd': 'r', 'id':'recordStart'},{'label': 'Stop', 'cmd': 's'}]}
-    ]},
-    {'title':'Tuner','cards':[
-      {'name':'Select Input','buttons':[{'label': 'Headset Mics', 'cmd': 'W', 'id':'configHeadset'},{'label': 'PCB Mics', 'cmd': 'w', 'id': 'configPCB'}]},
-      {'name':'Input Gain', 'buttons':[{'label': 'Less', 'cmd' :'I'},{'label': 'More', 'cmd': 'i'}]},
-      {'name':'Record Mics to SD Card','buttons':[{'label': 'Start', 'cmd': 'r', 'id':'recordStart'},{'label': 'Stop', 'cmd': 's'}]},
-      {'name':'CPU Reporting', 'buttons':[{'label': 'Start', 'cmd' :'c','id':'cpuStart'},{'label': 'Stop', 'cmd': 'C'}]}
-    ]}                            
-  ]
-};
-*/
 
 /**
  * This class contains the variables and methods for the Tympan Remote app.
