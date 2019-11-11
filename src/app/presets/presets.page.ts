ded7808f2f720ab91540b914891a3e55fd1b2267
Ken McEnaney <kxm@creare.com>
2019-11-11 14:51:43 -0500

    Move text input to presets page

--- src/app/presets/presets.page.ts
+++ src/app/presets/presets.page.ts
@@ -9,33 +9,13 @@ import { ButtonComponent } from './presetcomponents/button/button.component';
   styleUrls: ['presets.page.scss', '../prescription/prescription.page.scss']
 })
 export class PresetsPage {
+    textInput: string;
 
     constructor(public remote: TympanRemote, public logger:Logger, public buttonComp: ButtonComponent) {
     }
 
     cmd(s: string) {
         this.remote.send(s);
-        //this.applyStyleToBtnWithCmd(s);
     }
 
-    /*
-    applyStyleToBtnWithCmd(s: string) {
-    	for (let page of this.remote.pages) {
-    		for (let card of page.cards) {
-    			for (let button of card.buttons) {
-    				if (button.cmd == s) {
-    					console.log('FOUND BUTTON!');
-    					let str = 'STATE=' + 'BTN:' + button.id + ':';
-    					if (this.remote.btn[button.id].isOn) {
-	    					str += '0';
-    					} else {
-	    					str += '1';
-    					}
-    					this.remote.parseStateStringFromDevice(str);
-    				}
-    			}
-    		}
-    	}
-    }
-    */
 }
