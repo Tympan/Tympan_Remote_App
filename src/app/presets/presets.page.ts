b0711b89fac13ff8d4272d7b3dd38064c0940e6e
rnasser <rnasser@creare.com>
2019-12-31 10:07:45 -0500

    Added ability for serial plotter to detect number of datesets and put that many on the graph and for label names to be added if sent as first dataset from Tympan

--- src/app/presets/presets.page.ts
+++ src/app/presets/presets.page.ts
@@ -21,40 +21,11 @@ export class PresetsPage {
         this.remote.send(s);
     }
 
-    public async makeChart(layout) {
-      
-    }
-
-
     public makeSerialPlot() {
+      let lineC = undefined
       let lenDataset = undefined;
       let fullDatasets = []
       let myChart = undefined;
-      let xValue = 0;
-      let layout3 = {};
-      let layout2 = {
-        type: 'line',
-        data: {
-          datasets: []
-        },
-        options: {
-          scales: {
-            xAxes: [{
-              type: 'realtime',
-              realtime: {
-                onRefresh: onRefresh,
-                ttl: 60000
-              }
-            }],
-            yAxes: [{
-              scaleLabel: {
-                display: true,
-              }
-            }]
-          }
-        }
-      };
-
       var chartColors = [
         'rgb(255, 99, 132)',    //red
         'rgb(255, 159, 64)',    //orange
@@ -62,7 +33,8 @@ export class PresetsPage {
         'rgb(75, 192, 192)',    //green
         'rgb(54, 162, 235)',    //blue
         'rgb(153, 102, 255)',   //purple
-        'rgb(201, 203, 207)'    //grey
+        'rgb(201, 203, 207)',   //grey
+        'rgb(0, 0, 0)'          //black
       ];
 
       if (this.remote.bluetooth && this.remote.btSerial) {
@@ -84,15 +56,27 @@ export class PresetsPage {
         }
         if (lenDataset == undefined) {
           lenDataset = serialPlotData.length;
-          addDatasets(lenDataset);
+          if (typeof serialPlotData[0] == 'string'){
+            let labels = serialPlotData;
+            addDatasets(lenDataset, labels);
+          }
+          else {
+            addDatasets(lenDataset);
+          }
         }
       }
       
-      function addDatasets(data) {
-        let newDataset = {}
+      function addDatasets(data, labels = undefined) {
         for (let dset=0; dset<data; dset++) {
-          newDataset = {
-            label: 'Dataset ' + (dset + 1),
+          let dataName = undefined
+          if (labels != undefined){
+            dataName = labels[dset]
+          }
+          else {
+            dataName = 'Dataset ' + (dset + 1)
+          }
+          let newDataset = {
+            label: dataName,
             backgroundColor: "rgba(0, 0, 0, 0)",
             borderColor: chartColors[dset],
             fill: false,
@@ -101,8 +85,33 @@ export class PresetsPage {
           };
           fullDatasets.push(newDataset);
         }
-        layout2.data.datasets = fullDatasets;
-        layout3 = layout2;
+
+        var fullLayout = {
+          type: 'line',
+          data: {
+            datasets: fullDatasets
+          },
+          options: {
+            scales: {
+              xAxes: [{
+                type: 'realtime',
+                realtime: {
+                  onRefresh: onRefresh,
+                  ttl: 60000
+                }
+              }],
+              yAxes: [{
+                scaleLabel: {
+                  display: true,
+                }
+              }]
+            }
+          }
+        };
+
+        var canvas = <HTMLCanvasElement> document.getElementById('myChart');
+        var ctx = canvas.getContext('2d');
+        lineC = new Chart(canvas, fullLayout);
         return;
       };
       
@@ -111,71 +120,15 @@ export class PresetsPage {
         let set = 0;
         chart.config.data.datasets.forEach(function(dataset) {
           if (sData != undefined){
-          console.log('serial', sData)
           dataset.data.push({
             x: Date.now(),
             y: sData[set]
           });
           set += 1;
         }});
-        xValue += 1;
         myChart.update({preservation: true});
       }
 
-      var fullLayout = {
-        type: 'line',
-        data: {
-          datasets: [{
-            label: 'Dataset 1',
-            backgroundColor: "rgba(0, 0, 0, 0)",
-            borderColor: chartColors[0],
-            fill: false,
-            lineTension: 0,
-            data: []
-          },{
-            label: 'Dataset 2',
-            backgroundColor: "rgba(0, 0, 0, 0)",
-            borderColor: chartColors[1],
-            fill: false,
-            lineTension: 0,
-            data: []
-          },{
-            label: 'Dataset 3',
-            backgroundColor: "rgba(0, 0, 0, 0)",
-            borderColor: chartColors[2],
-            fill: false,
-            lineTension: 0,
-            data: []
-          }]
-        },
-        options: {
-          scales: {
-            xAxes: [{
-              type: 'realtime',
-              realtime: {
-                onRefresh: onRefresh,
-                ttl: 60000
-              }
-            }],
-            yAxes: [{
-              scaleLabel: {
-                display: true,
-              }
-            }]
-          }
-        }
-      };
-
-      function updateLabels(name, value) {
-        fullLayout.data.datasets[name].label = value;
-      }
-
-      var canvas = <HTMLCanvasElement> document.getElementById('myChart');
-      var ctx = canvas.getContext('2d');
-      // console.log('right before', layout3)
-      // if ('data' in layout3){
-      // console.log('in here')
-      this.lineCanvas = new Chart(canvas, fullLayout);
-      // }
+      this.lineCanvas = lineC
     }
 }
